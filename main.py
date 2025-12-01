a0886cb366185d75dd4f06e92972bca474348921
Luis Rubi <luisorubi@hotmail.com>
2025-12-01 08:44:12 -0600

    last change

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def initializeApp():
     print("ROMANIT-APP","\nBy LuisoRubiDev")
     user_num = input("\nIngresa el numero que quieres transformar: ")
-    print(user_num)
+    conver_num = int(user_num) + 5
+    print(conver_num)
 
 if __name__ == "__main__":
     initializeApp()
